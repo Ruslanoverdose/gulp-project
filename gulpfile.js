d05dc04fd9759b179b014aa811ada5b651e5ae1c
Ruslan Bekshenev <ruslanbekshenev@yandex.ru>
2019-09-06 23:37:35 +0500

    Gulp project with browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,62 +1,58 @@
-const gulp = require('gulp')
-const sass = require('gulp-sass')
-const watch = require('gulp-watch');
-const autoprefixer  =  require('gulp-autoprefixer')
-const cleanCss  =  require('gulp-clean-css')
-const concat  =  require('gulp-concat')
-const merge = require('merge-stream')
-const minify = require('gulp-minify')
-const imagemin = require('gulp-imagemin');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cleanCss = require('gulp-clean-css'),
+    concat = require('gulp-concat'),
+    merge = require('merge-stream'),
+    minify = require('gulp-minify'),
+    browserSync = require('browser-sync').create();
 
 //Таск на стили CSS, SASS
 function styles() {
-
-    const cssStream = gulp.src("src/css/*.css")
+    const cssStream = gulp.src("src/css/**/*.css")
         .pipe(concat('cssFiles.css'))
 
-    const sassStream = gulp.src("src/sass/*.sass")
-        .pipe(sass())
-        .pipe(concat('sassFiles.sass'))
-
-    const scssStream = gulp.src("src/scss/*.scss")
+    const scssStream = gulp.src("src/scss/**/*.scss")
         .pipe(sass())
         .pipe(concat('scssFiles.scss'))
 
-    const mergeStream = merge(cssStream, scssStream, sassStream )
+    const mergeStream = merge(cssStream, scssStream)
         .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
+            overrideBrowserslist: ['last 2 versions'],
             cascade: false
         }))
-        .pipe(concat('all.min.css'))
+        .pipe(concat('all.css'))
+        .pipe(autoprefixer())
         .pipe(cleanCss({
             compatibility: 'ie8'
         }))
-        .pipe(gulp.dest('build/css'))
+        .pipe(gulp.dest('assets/css'))
+        .pipe(browserSync.stream());
+
     return mergeStream
 }
 
 //Таск на скрипты JS
 function scripts() {
     return gulp.src("src/js/*.js")
-    .pipe(concat("scripts.js"))
-    .pipe(minify())
-    .pipe(gulp.dest("build/js"))
+        .pipe(concat("scripts.js"))
+        .pipe(minify())
+        .pipe(gulp.dest("assets/js"))
+        .pipe(browserSync.stream());
 }
 
-function images() {
-    const img = gulp.src('src/images/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('build/images/'))
-    return img
 
+function watcher() {
+    gulp.watch(['./src/css/**/*.css', './src/scss/**/*.scss'], styles)
+    gulp.watch('./src/js/**/*.js', scripts);
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    browserSync.reload();
 }
-gulp.task('styles', styles)
-gulp.task('scripts', scripts)
-gulp.task('callback', function () {
-    // Callback mode, useful if any plugin in the pipeline depends on the `end`/`flush` event
-    const css = watch(['src/css/**/*.css','src/sass/**/*.sass', 'src/scss/**/*.scss'], styles);
-    const js = watch('src/js/**/*.js', scripts);
-    const img = watch('src/images/*.*', images );
-    const mergeWatches = merge(css, js, img)
-    return mergeWatches
-});
+
+// gulp.task('sprite', gulp.series(sprite))
+gulp.task('callback', gulp.series(watcher));
